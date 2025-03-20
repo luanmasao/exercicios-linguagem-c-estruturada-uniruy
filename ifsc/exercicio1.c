36b660d22b46c37d320ec195ff6d99d597c8f71b
Luan <luanhirahata@hotmail.com>
2025-03-20 00:18:49 -0300

    Resolução do exercício 7 IFSC

---
+++ ifsc/exercicio1.c
@@ -0,0 +1,13 @@
+//questão 7 IFSC
+#include<stdio.h>
+
+int main()
+{
+    float AREA ;
+    float RAIO = 2.5;
+    AREA = 3.14159 * RAIO * RAIO;
+    
+    printf("Área da circunferência: %f", AREA);
+    
+    return 0;
+}
