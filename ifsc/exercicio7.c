c6da49549bca4cf785e0985e447a42e77c01b536
Luan <luanhirahata@hotmail.com>
2025-03-31 16:25:17 -0300

    Resolução exercício 70 IFSC

---
+++ ifsc/exercicio7.c
@@ -0,0 +1,18 @@
+//Questão 70 IFSC
+
+#include <stdio.h>
+
+int main()
+{
+    int input;
+
+    printf("Informe um número: ");
+    scanf("%d", &input);
+
+    if(input % 2 == 0)
+    {
+        printf("verdadeiro");
+    }
+
+    return 0;
+}
