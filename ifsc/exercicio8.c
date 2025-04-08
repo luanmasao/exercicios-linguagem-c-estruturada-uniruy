22eb3df7728345e9eae457e2409df6a71fb705ef
Luan <luanhirahata@hotmail.com>
2025-04-08 02:07:00 -0300

    Resolução exercício 76 IFSC

---
+++ ifsc/exercicio8.c
@@ -0,0 +1,23 @@
+//Questão 76 IFSC
+
+#include <stdio.h>
+
+int main()
+{
+    float massa_inicial;
+
+    printf("Informe a massa inicial em gramas: ");
+    scanf("%d", &massa_inicial);
+
+    float tempo = 0;
+
+    while (massa_inicial > 0.05)
+    {
+        massa_inicial /= 2.0;
+        tempo += 50;
+    }
+
+    printf("É necessário %d segundos para que a massa se torne menor que 0,05 gramas. ", tempo);   
+
+    return 0;
+} 
