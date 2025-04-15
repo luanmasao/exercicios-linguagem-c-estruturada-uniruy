1792062ed134665aa7f3b267f3659e649d1e3574
Luan <luanhirahata@hotmail.com>
2025-04-15 16:56:06 -0300

    Correção exercício 14 IFSC

--- ifsc/exercicio14.c
+++ ifsc/exercicio14.c
@@ -4,14 +4,15 @@
 
 int main()
 {
-    int input, x;
+    int input, x, produto = 1;
 
-    printf("Informe um número(Tabela de multiplicação de 1 a 13: \n");
+    printf("Informe um número(Tabela de multiplicação de 1 a 13): \n");
     scanf("%d", &input);
 
     for(x = 1; x <= 13; x++)
     {
-        printf("%d",x * input); 
+        produto = x * input;
+        printf("%d * %d = %d \n",x, input, produto); 
     }
 
     return 0;
