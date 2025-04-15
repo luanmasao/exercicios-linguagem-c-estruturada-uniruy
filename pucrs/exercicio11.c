dbc3303be7aab282c963b52d97c1a341729cc45c
Luan <luanhirahata@hotmail.com>
2025-04-15 18:14:24 -0300

    Correção exercício 13 PUCRS

--- pucrs/exercicio11.c
+++ pucrs/exercicio11.c
@@ -1,28 +1,28 @@
-//Questão 13 PUCRS
+//Questão 13 PUCRS  
 
 #include <stdio.h>
 
 int main()
 {
-    int input_inicial, contador_input = 0, input_secundario, fatorial, contador_fatorial ;
+    int input_inicial, contador_input = 1, input_secundario, fatorial, contador_fatorial;
 
     printf("Digite quantos valores devem ser lidos: \n");
     scanf("%d", &input_inicial);
 
-    while(contador_input <= input_inicial)
+    while (contador_input <= input_inicial) 
     {
         printf("Digite um número para descobrir seu fatorial: \n");
         scanf("%d", &input_secundario);
-        
-        for(contador_fatorial=1; contador_fatorial <= input_secundario; contador_fatorial++)
+
+        fatorial = 1; 
+
+        for (contador_fatorial = 1; contador_fatorial <= input_secundario; contador_fatorial++)
         {
-            fatorial = contador_fatorial * fatorial;
-            contador_input++;
-            printf("%dº número: %d\n", contador_input, fatorial);
+            fatorial *= contador_fatorial; 
         }
 
-
-        
+        printf("%dº número: %d! = %d\n", contador_input, input_secundario, fatorial); 
+        contador_input++;
     }
 
     return 0;
