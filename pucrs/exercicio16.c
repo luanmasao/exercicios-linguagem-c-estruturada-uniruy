b75867f29cad8f930a104bc37d65076d03a0471f
Luan <luanhirahata@hotmail.com>
2025-04-28 15:30:19 -0300

    Resolução exercício 42 PUCRS

---
+++ pucrs/exercicio16.c
@@ -0,0 +1,18 @@
+//Questão 42 PUCRS
+
+#include <stdio.h>
+
+int main()
+{
+    int n,h;
+
+    printf("Informe um número: ");
+    scanf("%d",&n);
+
+     h = n*10;
+
+    printf("Valor de H é: %d",h);
+
+    return 0;
+}
+
