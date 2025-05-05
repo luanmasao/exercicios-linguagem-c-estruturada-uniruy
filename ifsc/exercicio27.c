b30177c80296668fa3192dfb66d54271e79ce765
Luan <luanhirahata@hotmail.com>
2025-05-05 14:23:23 -0300

    Resolução exercício 126 IFSC

---
+++ ifsc/exercicio27.c
@@ -0,0 +1,19 @@
+//Questão 126 IFSC
+
+#include <stdio.h>
+
+int main()
+{
+    int vetor1[] = {5,2,4,1};
+    int vetor2[4];
+
+    for (int c = 0; c < 4; c++) {
+        vetor2[c] = vetor1[c];
+    }
+
+    for (int c = 0; c < 4; c++) {
+        printf("%d ", vetor2[c]);
+    }
+    
+}
+
