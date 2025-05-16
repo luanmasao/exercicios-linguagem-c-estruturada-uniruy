69abe49cc896300b636e73dc90aaaa16cbecd042
Luan <luanhirahata@hotmail.com>
2025-05-16 19:16:12 -0300

    Resolução exercício 46 IFSC

---
+++ ifsc/exercicio52.c
@@ -0,0 +1,32 @@
+//Questão 46 IFSC
+
+#include <stdio.h>
+
+int main()
+{
+    int nome[50];
+    int diarias;
+    float total;
+
+    printf("Informe o nome do cliente: ");
+    scanf("%s",nome);
+    printf("Informe a quantidade de diárias: ");
+    scanf("%d",diarias);
+
+    if(diarias > 15)
+    {
+        total = (diarias * 5.50) + (60 * diarias);
+    }
+    else if(diarias == 15)
+    {
+        total = (diarias * 6) + (60 * diarias);
+    }
+    else if(diarias < 15)
+    {
+        total = (diarias * 8) + (60 * diarias);
+    }
+
+    printf("Nome do cliente: %s. Total da conta: %.2f");
+
+    return 0;
+}
